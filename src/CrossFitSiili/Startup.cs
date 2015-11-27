569b6021c3011360caf946cfb42308a67127250a
nipery <niko.ryynanen@gmail.com>
2015-11-27 22:34:05 +0200

    MVC Enabled

--- src/CrossFitSiili/Startup.cs
+++ src/CrossFitSiili/Startup.cs
@@ -10,17 +10,25 @@ namespace CrossFitSiili
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+
             app.UseIISPlatformHandler();
 
-            app.UseStaticFiles();
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                  name: "Default",
+                  template: "{controller}/{action}/{id?}",
+                  defaults: new { controller = "Home", action = "Index" }
+                  );
+            });
 
-            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
 
         // Entry point for the application.
