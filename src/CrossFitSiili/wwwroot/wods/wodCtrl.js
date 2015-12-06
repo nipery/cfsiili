9b52546cea5692a0fb7c0821a4c754b7d10ed882
nipery <niko.ryynanen@gmail.com>
2015-12-06 15:13:26 +0200

    get wods implemented in wodController

--- src/CrossFitSiili/wwwroot/wods/wodCtrl.js
+++ src/CrossFitSiili/wwwroot/wods/wodCtrl.js
@@ -1,16 +1,40 @@
 (function () {
 
 
-    function wodCtrl() {
+    angular
+      .module('app.wods')
+      .controller('wodCtrl', wodCtrl);
+
+    wodCtrl.$inject = ['$state', '$http'];
+
+    function wodCtrl($state, $http) {
 
         var vm = this;
 
+        vm.wods = [{title:"foo", description:"bar"}];
+
         vm.title = 'new kind of Wodcontroller..';
+
+        activate();
+
+        function getWods() {
+
+            $http.get("/api/wods").then(function (response) {
+
+                angular.copy(response.data, vm.wods);
+                return vm.wods;
+
+            }, function (error) {
+
+                $scope.errorMessage = "Failed to load application list from server. " + error;
+            });
+        }
+
+        function activate() {
+
+            return getWods();
+        }
+
     }
-
-    angular
-  .module('app.wods')
-  .controller('wodCtrl', wodCtrl);
-
 
 })();
